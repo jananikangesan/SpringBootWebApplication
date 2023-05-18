b1eca816f4bdaa2fa94558d49e58d70f2e4350db
jananikangesan <janakanges1996@gmail.com>
2023-05-18 14:20:06 +0530

    changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -11,3 +15,14 @@ public class DemoApplication {
 	}
 
 }
+
+@RestController
+@RequestMapping(path="/")
+class TestController{
+
+	@RequestMapping(value="hello/{name}",method=RequestMethod.GET)
+	public String hello(@PathVariable(value ="name") String name){
+
+		return "Hello "+name;
+	}
+} 
