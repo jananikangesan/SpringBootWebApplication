1954930d82a3110d8bb92d8525fb21fbc9a40d09
jananikangesan <janakanges1996@gmail.com>
2023-05-29 11:45:17 +0530

    Creating Entities and Repositories

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,50 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String category;
+    private String type;
+    private String sector;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getSector() {
+        return sector;
+    }
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+ 
+}  
